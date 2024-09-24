81655b9576e222488259d808e82924925ff68025
barril01 <88594469+barril01@users.noreply.github.com>
2024-09-24 10:18:54 -0400

    Finished mod 4 assigment

--- Module_04/matlab/Q4_39.m
+++ Module_04/matlab/Q4_39.m
@@ -1,4 +1,4 @@
-%% EXERCISE 4.39
+%% EXERCi_1_hatSE 4.39
 % Gear A spins relative to its shaft, which rotates at variable rate Omega 1
 % about the horizontal axis. Gear B rotates at the variable rate Omega 2.
 % Determine the angular velocity and angular acceleration of gear A.
@@ -9,22 +9,29 @@ syms beta real
 syms gamma real
 syms L real
 
-I = [cos(beta+gamma);-sin(beta+gamma);0];
-J = [sin(beta+gamma); cos(beta+gamma);0];
+% i_1_hat = [cos(beta+gamma);-sin(beta+gamma);0];
+% J = [sin(beta+gamma); cos(beta+gamma);0];
 
-i_hat = [1;0;0];
-j_hat = [0;1;0];
+R = zRotT(-(beta+gamma));
 
-omega_bar = Omega_1*I + Omega_A*i_hat;
+i_2_hat = [1;0;0];
+j_2_hat = [0;1;0];
+
+i_1_hat = R*i_2_hat;
+j_1_hat = R*j_2_hat;
+
+
+omega_bar = Omega_1*i_1_hat + Omega_A*i_2_hat;
 
 Omega_21 = Omega_2-Omega_1;
 
-Omega_A = solve(simplify(cross(Omega_A*[1;0;0],L*sin(gamma)/cos(beta)*[0;-1;0]) == ...
-    cross(Omega_21*I,L*tan(beta)*J)),Omega_A);
+%% 
+Omega_A = solve(simplify(cross(Omega_A*i_2_hat,L*sin(gamma)/cos(beta)*(-j_2_hat)) == ...
+    cross(Omega_21*i_1_hat,L*tan(beta)*j_1_hat)),Omega_A);
 
-omega_bar = Omega_1*I + Omega_A*i_hat;
+omega_bar = Omega_1*i_1_hat + Omega_A*i_2_hat;
 
-alpha_bar = diff(Omega_1,t)*I +...
-            cross(Omega_1*I,Omega_1*I) +...
-            diff(Omega_A,t)*i_hat +...
-            cross(omega_bar,Omega_A*i_hat);
+alpha_bar = diff(Omega_1,t)*i_1_hat +...
+            cross(Omega_1*i_1_hat,Omega_1*i_1_hat) +...
+            diff(Omega_A,t)*i_2_hat +...
+            cross(omega_bar,Omega_A*i_2_hat);
