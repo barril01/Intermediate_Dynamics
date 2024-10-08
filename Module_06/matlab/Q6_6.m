0565797fd4c96e0f88021756ff685b9a68c62a6e
barril01 <88594469+barril01@users.noreply.github.com>
2024-10-07 22:50:34 -0400

    further module 6.

--- Module_06/matlab/Q6_6.m
+++ Module_06/matlab/Q6_6.m
@@ -5,11 +5,34 @@
 % servomotor. Let I1 denote the moment of inertia of the flywheel about
 % axis AB, and let I2 be the centroidal moment of inertia perpendicular
 % to axis AB. Derive the differential equation of motion for β.
-clc, clear
+clc,clear
+syms k t beta(t)
 
-syms Omega_1 Omega_2 t
-syms beta(t)
+syms Omega_1 Omega_2(t)
 
-R = Dynamics.Inertial2Body.zRot(beta);
+syms I_1 I_2
 
-omega_bar = Omega_2*R*[0;1;0] + diff(beta,t)*[0;1;0] + Omega_2*[10;0;0];
+R = yRot(-beta);
+
+omega_bar = Omega_2*R*[0;0;1] + diff(beta,t)*[0;-1;0]...
+                + Omega_1*[-1;0;0];
+
+Omega_N = Omega_2(t)*R(t)*[0;0;1];
+
+alpha_bar = cross(omega_bar,Omega_1*[-1;0;0]) +...
+            diff(Omega_2,t)*R*[0;0;1] +...
+            simplify(cross(Omega_N,Omega_2*R*[0;0;1])) +...
+            diff(diff(beta,t),t)*[0;-1;0] +...
+            cross( Omega_N ,diff(beta,t)*[0;-1;0]);
+
+% ob = Omega_2(t)*R(t)*[0;0;1];
+
+system = ([0;1;0]*k*beta(t) == I_2*alpha_bar(t)-(I_2-I_1)*Omega_N(1)*Omega_N(3));
+
+system(2)
+
+function R = yRot(ang)
+R = [ cos(ang) 0 -sin(ang) ; %% Neg body 2 in
+0 1 0 ;
+sin(ang) 0 cos(ang) ];
+end
