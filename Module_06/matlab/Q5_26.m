8fb3f6196c341f1bfab2f4e7fa7c430dca1c48fa
barril01 <88594469+barril01@users.noreply.github.com>
2024-10-09 21:38:51 -0400

    Submitted Mod 6. Mod 7 ready to get started.

--- Module_06/matlab/Q5_26.m
+++ Module_06/matlab/Q5_26.m
@@ -1,12 +1,13 @@
 %% EXERCISE 5.26
-% Thin bar ACB is welded to a shaft that rotates at the constant angular speed,
-% so the angle θ between the bar and the shaft is constant.
-% (a) Derive expressions for the angular momentum H ̄C and the
-%     kinetic energy of the bar. Draw a sketch of H ̄C.
-% (b) Based on an analysis of the manner in which H ̄C in Part (a)
-%     rotates, derive an expression for dH ̄C/dt.
-% (c) Use Eq. (5.3.4) to evaluate dH ̄C/dt, and compare it with the
-%     result of Part (b).
+% Thin bar ACB is welded to a shaft that rotates at the constant
+% angular speed, so the angle theta between the bar and the
+% shaft is constant.
+% (a) Derive expressions for the angular momentum H-C and the
+%     kinetic energy of the bar. Draw a sketch of H-C.
+% (b) Based on an analysis of the manner in which H-C in Part (a)
+%     rotates, derive an expression for dH-C/dt.
+% (c) Use Eq. (5.3.4) to evaluate dH -C/dt, and compare it
+%     with the result of Part (b).
 
 syms m L t
 syms theta real
@@ -23,7 +24,3 @@ I_pa = Dynamics.Inertia.parallelAxisTransform(m,[(2*L/3-L/2),0,0]);
 H_C = (I+I_pa)*omega_bar;
 
 H_C_dot = diff(H_C,t) + cross(omega_bar,H_C);
-
-
-
-
--- Module_06/matlab/Q6_8.m
+++ Module_06/matlab/Q6_8.m
@@ -1,10 +1,12 @@
 %% EXERCISE 6.8
 % The torque M acting on the gimbal of the gyroscopic turn indicator is
-% exerted by a torsional spring, so M = −kβ. The precession rate 2 is a
-% specified function of time, and the spin rate 1 is held constant by a
-% servomotor. Let I1 denote the moment of inertia of the flywheel about
-% axis AB, and let I2 be the centroidal moment of inertia perpendicular
-% to axis AB. Derive the differential equation of motion for β.
+% exerted by a torsional spring, so M = -k beta. The precession rate
+% Omega 2 is a specified function of time, and the spin rate Omega 1 is
+% held constant by a servomotor. Let I1 denote the moment of inertia of
+% the flywheel about axis AB, and let I2 be the centroidal moment of
+% inertia perpendicular to axis AB. Derive the differential equation
+% of motion for beta.
+
 clc,clear
 syms k t beta(t)
 
@@ -25,9 +27,8 @@ alpha_bar = cross(omega_bar,Omega_1*[-1;0;0]) +...
             diff(diff(beta,t),t)*[0;-1;0] +...
             cross( Omega_N ,diff(beta,t)*[0;-1;0]);
 
-% ob = Omega_2(t)*R(t)*[0;0;1];
-
-system = ([0;1;0]*k*beta(t) == I_2*alpha_bar(t)-(I_2-I_1)*Omega_N(1)*Omega_N(3));
+system = ([0;1;0]*k*beta(t) == I_2*alpha_bar(t)-...
+    (I_2-I_1)*Omega_N(1)*Omega_N(3));
 
 system(2)
 
