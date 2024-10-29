b96f75d38d0ce4f35e3089fdecf865b88b27d36f
barril01 <88594469+barril01@users.noreply.github.com>
2024-10-28 21:22:34 -0400

    mostly done mod 9

--- Module_09/matlab/Problem_2.m
+++ Module_09/matlab/Problem_2.m
@@ -1,16 +1,16 @@
 clc,clear
-syms t m L k_p a
+syms t m L k_p a X(t)
 syms theta(t)
 % Position
-x = L*sin(theta);
-y = L*cos(theta);
+y = L*sin(theta);
+x = L*cos(theta)+X;
 % velocity
 x_dot = diff(x,t);
 y_dot = diff(y,t);
 % Kinetic Energy
 T = 0.5*m*(simplify(expand(x_dot^2 + y_dot^2)));
 % Kinetic energy from spring
-V = 0.5*k_p*theta^2 + m*a*y;
+V = 0.5*k_p*theta^2 + m*(-a)*x;
 % Lagrange Function
 L = T - V;
 % Coordinate derivatives
@@ -25,4 +25,4 @@ EOM = simplify(dL_dtheta_dot_dt-dL_dtheta);
 syms P(t); assume(P(t),'real')
 r = [x;y];
 dr_dtheta = diff(r,theta);
-F = (P*[0;-1])' * dr_dtheta; % dot product
+F = (P*[-1;0])' * dr_dtheta; % dot product
